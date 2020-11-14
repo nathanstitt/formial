017b6fa7a0f807486ffd1e721559372688d9d4d6
Nathan Stitt <nathan@stitt.org>
2020-11-13 22:14:14 -0600

    Updates

---
+++ precache-manifest.ebe0fa275ca3c8055f70dcc7f9513d09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82448d1d69227e1fb8572fff6bdf1862",
+    "url": "./index.html"
+  },
+  {
+    "revision": "95dda681315ef104605c",
+    "url": "./static/css/main.97545450.chunk.css"
+  },
+  {
+    "revision": "e748ac60b32869e24af7",
+    "url": "./static/js/2.4589835d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "./static/js/2.4589835d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95dda681315ef104605c",
+    "url": "./static/js/main.61f682d1.chunk.js"
+  },
+  {
+    "revision": "4b4d47ff2c967da48168",
+    "url": "./static/js/runtime-main.d174ad6f.js"
+  }
+]);
